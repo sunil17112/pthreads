86a4a284b8f3969e1faf360357eec08d8da42666
piyus kedia <piyuskedia@gmail.com>
2020-03-23 20:22:55 +0530

    Initial commit

---
+++ carrace.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <pthread.h>
+
+// you can change the prototype of existing
+// functions, add new routines and global variables
+// cheatmode, car1, car2, report are different threads
+// they communicate with each other via shared memory
+
+
+// the cheatmode thread
+void *cheatmode(void *arg)
+{
+	return NULL;
+}
+
+// the car1 thread
+void *car1(void *arg)
+{
+	return NULL;
+}
+
+// the car2 thread
+void *car2(void *arg)
+{
+	return NULL;
+}
+
+// the report thread
+void *report(void *arg)
+{
+	return NULL;
+}
+
+// create all threads
+int main()
+{
+	return 0;
+}

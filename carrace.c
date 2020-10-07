0983d98d377ee8e52a4b6243ed611c59f6e9ec77
sunil17112 <52931377+sunil17112@users.noreply.github.com>
2020-10-07 22:20:44 +0530

    create pthread

--- carrace.c
+++ carrace.c
@@ -33,9 +33,43 @@ void *report(void *arg)
 {
 	return NULL;
 }
+// it create all threads and initialize all locks
+void create_all_thread(){
+
+	pthread_mutex_init(&car1_lock , NULL);
+	pthread_mutex_init(&car2_lock , NULL);
+	pthread_mutex_init(&car1_user_lock , NULL);
+	pthread_mutex_init(&car2_user_lock , NULL);
+
+
+	pthread_create(&cheatmode_thread , NULL , &cheatmode , NULL);
+	pthread_create(&car1_thread , NULL , &car1 , NULL);
+	pthread_create(&car2_thread , NULL , &car2 , NULL);
+	
+}
+
+// it join all threads and destroy all locks
+void join_all_thread(){
+	pthread_join(cheatmode_thread , NULL);
+	pthread_join(car1_thread , NULL);
+	pthread_join(car2_thread , NULL);
+	
+
+	pthread_mutex_destroy(&car1_lock );
+	pthread_mutex_destroy(&car2_lock);
+	pthread_mutex_destroy(&car1_user_lock);
+	pthread_mutex_destroy(&car2_user_lock);
+
+}
 
 // create all threads
 int main()
 {
+
+	create_all_thread(); // create all pthreads
+	report(0); // using master / main thread for report component as a fourth thread
+
+	join_all_thread(); // join all pthreads
+
 	return 0;
 }
